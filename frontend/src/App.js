0638fdc4ed031ed671f09af687a2b7c3d08f7360
shirley1296 <shirleymushi1296@gmail.com>
2022-03-11 17:05:08 -0500

    React Bootstrap bootswatch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
  
 
 // arrow function
 const App = () => {
   return (
     <>
+    <Header/>
+  <main>
+    <Container>
    <h1>Welcome to my shop</h1> 
+   </Container>
+
+   </main>
+  <Footer/>
     </>
   );
 
